d49a57c7467425b5da57f4e0424a02ef2558463d
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-03-13 16:22:06 +0000

    generuje losowe liczby

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Random;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    Random randomNumbers = new Random();
+   
+
+    
+
+    
+  
+    System.out.println("losowa liczba " + (randomNumbers.nextInt(100)+1) );
+    System.out.println("losowa liczba " + (randomNumbers.nextInt(6)+1) );
+ 
+
+   
   }
 
   // @Test
